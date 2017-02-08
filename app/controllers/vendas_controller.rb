9df19ed45f362bd463a39fc68d8bea8bb2202d4e
Daniel WS <danielws.x64@gmail.com>
2017-02-08 15:38:43 -0200

    Refactor last commit

--- app/controllers/vendas_controller.rb
+++ app/controllers/vendas_controller.rb
@@ -1,9 +1,9 @@
 class VendasController < ApplicationController
   PRODUCTS_LIST = [
-    {name: 'Hospedagem', id: 1},
-    {name: 'Registro de Dominios', id: 2},
-    {name: 'Email Marketing', id: 3}
-  ]
+    { name: 'Hospedagem', id: 1 },
+    { name: 'Registro de Dominios', id: 2 },
+    { name: 'Email Marketing', id: 3 }
+  ].freeze
 
   def index
     @products = PRODUCTS_LIST
